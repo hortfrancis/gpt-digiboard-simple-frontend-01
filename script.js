7bbdd29fe9cf95fff14416ba9d3c1d124efe7924
Alex Hort-Francis <60655956+hortfrancis@users.noreply.github.com>
2024-04-15 22:09:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const main = document.querySelector('main'); 
+
+function getMessage() {
+  fetch('https://gpt-digiboard-backend-01.onrender.com/message')
+    .then(response => response.json())
+    .then(data => {
+      console.log('data:', data); 
+      main.innerText = data;
+    });
+}
+
+// Get the message when the page loads 
+getMessage();
+
+// Then every 5 seconds 
+setInterval(() => {
+  getMessage()
+}, 5000);
